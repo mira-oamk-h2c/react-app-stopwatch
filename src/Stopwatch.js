21e33ab4fe08b96721cee544baca04932b197ce5
Mira Eerikäinen <c0eemi00@students.oamk.fi>
2021-03-23 13:10:59 +0200

    second commit

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import Toggle from './Toggle';
 
 
-export default class Clock extends Component {
+export default class Stopwatch extends Component {
   constructor(props) {
     super(props);
     this.state = { seconds: 0 };
